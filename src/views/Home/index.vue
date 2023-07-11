af1a43bfd077a4b3ed7d441c5aa0a584079f3b7c
ExecuteMyExecution <771849622@qq.com>
2023-07-11 23:07:29 +0800

    添加tabs组件和分页组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="container">
-        <el-table :data="stuInfo" border stripe style="width: 100%">
-            <el-table-column v-for="(item, index) of showInfo" :key="index" :prop="item" :label="item" />
-        </el-table>
+        <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+            <el-tab-pane label="现役" name="now">
+                now
+                <el-table :data="stuInfo" border stripe style="width: 100%">
+                    <el-table-column type="selection" width="55" />
+                    <el-table-column v-for="(item, index) of showInfo" :key="index" :prop="item" :label="item" />
+                </el-table>
+            </el-tab-pane>
+            <el-tab-pane label="退役" name="before">
+                before
+                <el-table :data="stuInfo" border stripe style="width: 100%">
+                    <el-table-column type="selection" width="55" />
+                    <el-table-column v-for="(item, index) of showInfo" :key="index" :prop="item" :label="item" />
+                </el-table>
+            </el-tab-pane>
+            <el-tab-pane label="所有" name="all">
+                all
+                <el-table :data="stuInfo" border stripe style="width: 100%">
+                    <el-table-column type="selection" width="55" />
+                    <el-table-column v-for="(item, index) of showInfo" :key="index" :prop="item" :label="item" />
+                </el-table>
+            </el-tab-pane>
+        </el-tabs>
+        <el-pagination background layout="prev, pager, next" :total="1000" />
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 const showInfo = [
     "stuNO",
     "realname",
@@ -79,6 +102,8 @@ const stuInfo = [
         "jsk": 0
     }
 ]
+
+const activeName = ref('now');
 </script>
 
 <style lang="scss" scoped>
@@ -86,5 +111,10 @@ const stuInfo = [
     padding: 30px;
     background-color: #fff;
     box-shadow: 0 0 5px 1px #ccc;
+    .el-pagination {
+        margin-top: 20px;
+        display: flex;
+        justify-content: end;
+    }
 }
 </style>
